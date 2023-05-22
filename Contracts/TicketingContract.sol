e21acdc202fdadeb052c4f930736037501308696
SleepyPopcorn <108483647+SleepyPopcorn@users.noreply.github.com>
2023-05-22 19:55:47 +0300

    added variables, constructor and the visitor struct

--- Contracts/TicketingContract.sol
+++ Contracts/TicketingContract.sol
@@ -1,33 +1,52 @@
 pragma solidity >=0.7.0 <0.9;
 
 contract TicketingSystem {
-    // Organizor needs to create smart contract and put it on the 
-    // Etherium Blockchain and share its address
 
-    // Ornaizer gives; 
-    // -- Wallet İnfo of Ornazier, 
-    // -- Ticket Price
-    // -- visitor count
-    // -- event start date
-    // -- event finish date
-    // also derive minimum event tiem and expritarion date
+    address payable internal organizer;
 
+    uint public ticketPrice;
+    uint public eventStartTime;
+    uint public eventFinishTime;
+    uint public visitorLimit;
 
+    uint internal eventMinTime;
+    uint internal expirationTime;
 
-    // visitor ticket buy function ;
-    // visitor name , surname and wallet info
+    mapping (address => uint256) public addressToHeldWei;
 
+    event eventCancelled(string errorMessage);
 
-    // organizer name check function 
-    // also count when the visitor attend (inside of this function)
+    constructor(uint _ticketPrice, uint _visitorLimit, uint _eventStartTime, uint _eventFinishTime, uint _expirationTime){
+        organizer = payable(msg.sender);
 
+        ticketPrice = _ticketPrice;
+        visitorLimit = _visitorLimit;
+        eventStartTime = _eventStartTime;
+        eventFinishTime = _eventFinishTime;
+        expirationTime = _expirationTime;
 
-    // when visitor leave he needs to *** Check oracle then decide***
+        eventMinTime = ( (eventFinishTime - eventStartTime) * 30) / 100;
+        expirationTime = eventFinishTime + 1 days;
+    }
 
-    // while checking leaving ppl check ratio of stayed/attended
+    struct Visitor{
+        string name;
+        string surname;
+        address payable wallet;
+    }
 
+    Visitor[] public visitors;
 
-    // at expiration date ; if condiction are ok, do the transaction if not refund.
+    modifier checkSeats(){
+        require(visitors.length < visitorLimit);
+        _;
+    }
 
+    modifier checkTime(){
+        require(block.timestamp < eventStartTime);
+        _;
+    }
+
+    
 
 }
