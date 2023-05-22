a00f1217a300288b462ed0b3b347907a5ef7b9dc
sreaws <bugrahan_hazman@hotmail.com>
2023-05-22 20:17:42 +0300

    Added second half
    Modifiers , functions etc.

--- Contracts/TicketingContract.sol
+++ Contracts/TicketingContract.sol
@@ -13,7 +13,7 @@ contract TicketingSystem {
 
     mapping (address => uint256) public addressToHeldWei;
 
-    event eventCancelled(string errorMessage);
+    event EventCancelled(string errorMessage);
 
     constructor(uint _ticketPrice, uint _visitorLimit, uint _eventStartTime, uint _eventFinishTime){
         organizer = payable(msg.sender);
@@ -44,5 +44,50 @@ contract TicketingSystem {
     // when visitor leave he needs to *** Check oracle then decide***
 
     // while checking leaving ppl check ratio of stayed/attended
+    modifier checkSeats() {
+        require(visitors.length < visitorLimit);
+        _;
+    }
 
+    modifier checkTime() {
+        require(block.timestamp < eventStartTime);
+        _;
+    }
+
+    function buyTicket (string memory _name, string memory _surname) public payable checkSeats() checkTime() {
+        require(msg.value == ticketPrice, "Wrong ticket price entered. Please check the value entered and try again.");
+        visitors.push(Visitor(_name, _surname, payable(msg.sender)));
+        addressToHeldWei[msg.sender] += msg.value;
+    }
+
+    modifier onlyOrganizer {
+        require(msg.sender == organizer);
+        _;
+    }
+
+    function refundAllWei () private {
+        for (uint i = 0; i < visitors.length; i++) {
+            visitors[i].wallet.transfer(ticketPrice);
+        }
+        emit EventCancelled("The event is cancelled");
+    }
+
+    function cancelEvent() public onlyOrganizer(){
+        refundAllWei();
+    }
+
+    function isCancelled() private returns (bool) {
+
+    }
+
+    function contractExpiration() public { // think how to call this.
+        require(block.timestamp > expirationTime, "The Contract is not expired yet!");
+        if (isCancelled()){
+            refundAllWei();
+        }
+        else{
+            organizer.transfer(address(this).balance);
+        }
+
+    }
 }
