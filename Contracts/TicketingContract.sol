fb42316731e1fc4711eea146701d2ab7816075de
sreaws <bugrahan_hazman@hotmail.com>
2023-05-22 15:36:46 +0300

    General Structure
    Described the basic steps of the ticketing systems with comment lines

--- Contracts/TicketingContract.sol
+++ Contracts/TicketingContract.sol
@@ -1,1 +1,33 @@
 pragma solidity >=0.7.0 <0.9;
+
+contract TicketingSystem {
+    // Organizor needs to create smart contract and put it on the 
+    // Etherium Blockchain and share its address
+
+    // Ornaizer gives; 
+    // -- Wallet İnfo of Ornazier, 
+    // -- Ticket Price
+    // -- visitor count
+    // -- event start date
+    // -- event finish date
+    // also derive minimum event tiem and expritarion date
+
+
+
+    // visitor ticket buy function ;
+    // visitor name , surname and wallet info
+
+
+    // organizer name check function 
+    // also count when the visitor attend (inside of this function)
+
+
+    // when visitor leave he needs to *** Check oracle then decide***
+
+    // while checking leaving ppl check ratio of stayed/attended
+
+
+    // at expiration date ; if condiction are ok, do the transaction if not refund.
+
+
+}
