791851fa7691bbf64e589c83cef3a84c130026f9
SleepyPopcorn <108483647+SleepyPopcorn@users.noreply.github.com>
2023-06-10 22:31:16 +0300

    After presentation changes

--- Contracts/TicketingContract.sol
+++ Contracts/TicketingContract.sol
@@ -1,227 +1,244 @@
-pragma solidity >=0.7.0 <0.9;
 
-contract TicketingSystem {
-    
-    address payable internal organizer;
 
-    //* Publicly accessable variables 
-    uint public ticketPrice;
-    uint public visitorLimit;
-    uint public eventStartTime;
-    uint public eventFinishTime;
-    
-    //* Internal derived variables
-    uint internal eventMinTime;
-    uint internal expirationTime;
+    pragma solidity >=0.7.0 <0.9;
 
-    //* Private visitor status counts
-    uint private stayed;
-    uint private attended;
-    uint private notAttended;
+    contract TicketingSystem {
+        
+        address payable internal organizer;
 
-    //* To Store wei
-    mapping (address => uint256) private addressToHeldWei;
+        //* Publicly accessable variables 
+        uint public ticketPrice;
+        uint public visitorLimit;
+        uint public eventStartTime;
+        uint public eventFinishTime;
+        
+        //* Internal derived variables
+        uint internal eventMinTime;
+        uint internal expirationTime;
 
-    //* Events
-    event EventCancelled(string errorMessage);
+        //* Private visitor status counts
+        uint private stayed;
+        uint private attended;
+        uint private notAttended;
 
-    //* Status of the contract
-    enum EventStatus {ONGOING, FINISHED, CANCELLED}
-    EventStatus private statusOfEvent;
+        //* To Store wei
+        mapping (address => uint256) private addressToHeldWei;
 
-    enum VisitorStatus {NOT_ATTENDED, ATTENDED, STAYED}
-    VisitorStatus private statusOfVisitor;
+        //* Events
+        event EventCancelled(string errorMessage);
 
-    constructor(uint _ticketPrice, uint _visitorLimit, uint _eventStartTime, uint _eventFinishTime){
-        organizer = payable(msg.sender);
+        //* Status of the contract
+        enum EventStatus {ONGOING, FINISHED, CANCELLED}
+        EventStatus private statusOfEvent;
 
-        ticketPrice = _ticketPrice;
-        visitorLimit = _visitorLimit;
-        eventStartTime = _eventStartTime;
-        eventFinishTime = _eventFinishTime;
+        //* Status of the visitors
+        enum VisitorStatus {NOT_ATTENDED, ATTENDED, STAYED}
+        VisitorStatus private statusOfVisitor;
 
-        eventMinTime = eventStartTime + ((eventFinishTime - eventStartTime) * 30) / 100;
-        expirationTime = eventFinishTime ;//+ 1 days; 
-        //***************************     ^^^^^^^^ dont forget to fix */
+        constructor(uint _ticketPrice, uint _visitorLimit, uint _eventStartTime, uint _eventFinishTime){
+            organizer = payable(msg.sender);
 
-        statusOfEvent = EventStatus.ONGOING;
-    }
+            ticketPrice = _ticketPrice;
+            visitorLimit = _visitorLimit;
+            eventStartTime = _eventStartTime;
+            eventFinishTime = _eventFinishTime;
 
-    //* To Store Visitors
-    struct Visitor{
-        string name;
-        string surname;
-        address payable walletAddress;
-        VisitorStatus status;
-    }
+            eventMinTime = eventStartTime + ((eventFinishTime - eventStartTime) * 30) / 100;
+            expirationTime = eventFinishTime + 1 days; 
 
-    Visitor[] private visitors;
-
-    // ************************
-    // ****** MODIFIERS *******
-    // ************************
-
-    modifier statusCheck() {
-        if (statusOfEvent == EventStatus.CANCELLED) {
-            revert("The Event is Cancelled. You can not use this The Contract anymore");
+            statusOfEvent = EventStatus.ONGOING;
         }
-        else if (statusOfEvent == EventStatus.FINISHED){
-            revert("The Event is Over. You can not use this The Contract anymore");
-        }       
-        _;
-    }
-    
-    modifier checkSeats(){
-        require(visitors.length < visitorLimit, "All tickets are taken");
-        _;
-    }
 
-    modifier checkEventStartTime(){
-        require(block.timestamp < eventStartTime, "The event is already started! You cannot buy tickets");
-        _;
-    }
+        //* To Store Visitors
+        struct Visitor{
+            uint256 ID;
+            address payable walletAddress;
+            VisitorStatus status;
+        }
 
-    modifier onlyOrganizer(){
-        require(msg.sender == organizer, "This function is Organizer Only!");
-        _;
-    }
+        Visitor[] private visitors;
 
-    modifier onlyOracle(){
-        require(msg.sender == oracleAddress, "This function is not open for everyone");
-        _;
-    }
+        // ************************
+        // ****** MODIFIERS *******
+        // ************************
 
-    // *******************************
-    // ****** HELPER FUNCTIONS *******
-    // *******************************
+        modifier statusCheck() {
+            if (statusOfEvent == EventStatus.CANCELLED) {
+                revert("The Event is Cancelled. You can not use this The Contract anymore");
+            }
+            else if (statusOfEvent == EventStatus.FINISHED){
+                revert("The Event is Over. You can not use this The Contract anymore");
+            }       
+            _;
+        }
+        
+        modifier checkSeats(){
+            require(visitors.length < visitorLimit, "All tickets are taken");
+            _;
+        }
 
-    uint testFLAG = 0;
-    function calculateRatios() private {
-        testFLAG = visitors.length;
-        for (uint i = 0; i < visitors.length; i++) {
-            if (visitors[i].status == VisitorStatus.STAYED)
-                stayed++;
-            else if(visitors[i].status == VisitorStatus.ATTENDED)
-                attended++;
+        modifier checkEventStartTime(){
+            require(block.timestamp < eventStartTime, "The event is already started! You cannot buy tickets");
+            _;
+        }
+
+        modifier onlyOrganizer(){
+            require(msg.sender == organizer, "This function is Organizer Only!");
+            _;
+        }
+
+        modifier onlyOracle(){
+            require(msg.sender == oracleAddress, "This function is not open for everyone");
+            _;
+        }
+
+        // *******************************
+        // ****** HELPER FUNCTIONS *******
+        // *******************************
+
+        uint testFLAG = 0;
+        function calculateRatios() private {
+            testFLAG = visitors.length;
+            for (uint i = 0; i < visitors.length; i++) {
+                if (visitors[i].status == VisitorStatus.STAYED)
+                    stayed++;
+                else if(visitors[i].status == VisitorStatus.ATTENDED)
+                    attended++;
+                else 
+                    notAttended++;
+            }
+        }
+
+        function checkStayedOverAttendedRatio() private view returns(bool){
+            if (stayed >= attended)
+                return true;
             else 
-                notAttended++;
-        }
-    }
-
-    function checkStayedOverAttendedRatio() private view returns(bool){
-        if (stayed >= attended)
-            return true;
-        else 
-            return false;
-    }
-
-    function checkAttendedOverNotAttended() private view returns(bool){
-        if (attended + stayed >= notAttended)
-            return true;
-        else    
-            return false;
-    }
-
-    function refundAllWei () private{
-        for (uint i = 0; i < visitors.length; i++) {
-            visitors[i].walletAddress.transfer(ticketPrice);
-        }
-       statusOfEvent = EventStatus.CANCELLED;
-        emit EventCancelled("The event is cancelled");
-    }
-
-    function isCancelled() private view returns (bool){
-
-        if (checkStayedOverAttendedRatio() && checkAttendedOverNotAttended()) {
-            return false;
+                return false;
         }
 
-        return true;
-    }
+        function checkAttendedOverNotAttended() private view returns(bool){
+            if (attended + stayed >= notAttended)
+                return true;
+            else    
+                return false;
+        }
 
-    function checkNameSurname(string calldata _name, string calldata _surname) private view returns(uint){
-        uint result = visitorLimit + 1;
-        for(uint i = 0; i < visitors.length; i++) {
-            if (keccak256(abi.encodePacked(visitors[i].name)) == keccak256(abi.encodePacked(_name))
-            && keccak256(abi.encodePacked(visitors[i].surname)) == keccak256(abi.encodePacked(_surname))) { 
-                result = i;
-                break;
+        function refundAllWei () private{
+            for (uint i = 0; i < visitors.length; i++) {
+                visitors[i].walletAddress.transfer(ticketPrice);
             }
+        statusOfEvent = EventStatus.CANCELLED;
+            emit EventCancelled("The event is cancelled");
         }
-        return result;
-    }
 
-    function checkAddress(address _visitorAddress) private view returns (bool) {
-        bool result = true;
-        for(uint i = 0; i < visitors.length; i++) {
-            if (visitors[i].walletAddress == _visitorAddress) { 
-                result = false;
-                break;
+        function isCancelled() private view returns (bool){
+
+            if (checkStayedOverAttendedRatio() && checkAttendedOverNotAttended()) {
+                return false;
             }
-        }
-        return result;
-    }
 
-    // *******************************
-    // ****** PUBLIC FUNCTIONS *******
-    // *******************************
-    
-    // Oracle Function
-    function visitorLeaveCheck(string calldata _name, string calldata _surname) public statusCheck() onlyOracle() returns(string memory){
-        uint index = checkNameSurname(_name, _surname);
-        require(index != visitorLimit + 1, "Invalid name or surname. Please try again!");
-        if (eventMinTime < block.timestamp){
-            visitors[index].status = VisitorStatus.STAYED;
+            return true;
         }
 
-        return "The Visitor is marked as STAYED";
-    }
+        function checkNameSurnameEntrance(uint256 _userID) private view returns(uint){
+            uint result = visitorLimit + 1;
+            for(uint i = 0; i < visitors.length; i++) {
+                if (keccak256(abi.encodePacked(visitors[i].ID)) == keccak256(abi.encodePacked(_userID))
+                && visitors[i].status == VisitorStatus.NOT_ATTENDED) { 
+                    result = i;
+                    break;
+                }
+            }
+            return result;
+        }
 
-    // Oracle Function
-    function visitorEnterCheck(string calldata _name, string calldata _surname) public statusCheck() onlyOracle() returns(string memory){
-        uint index = checkNameSurname(_name, _surname);
-        require(index != visitorLimit + 1, "-- The name is not found on the list. --");
-        visitors[index].status = VisitorStatus.ATTENDED;
-        return "++ The name is on the list. The Visitor is marked as ATTENDED ++";
-    }
-    
-    function buyTicket (string calldata _name, string calldata _surname) public payable statusCheck() checkSeats() checkEventStartTime() {
-        require(msg.value == ticketPrice, "Wrong ticket price entered. Please check the value entered and try again.");
-        require(checkAddress(msg.sender), "Only one ticket is allowed for per address");
-        visitors.push(Visitor(_name, _surname, payable(msg.sender), VisitorStatus.NOT_ATTENDED));
-        addressToHeldWei[msg.sender] += msg.value;
-    }
+        function checkNameSurnameExit(uint256 _userID) private view returns(uint){
+            uint result = visitorLimit + 1;
+            for(uint i = 0; i < visitors.length; i++) {
+                if (keccak256(abi.encodePacked(visitors[i].ID)) == keccak256(abi.encodePacked(_userID))
+                && visitors[i].status != VisitorStatus.STAYED) { 
+                    result = i;
+                    break;
+                }
+            }
+            return result;
+        }
 
-    function cancelEvent() public statusCheck() onlyOrganizer(){
-        refundAllWei();
-    }
+        function checkAddress(address _visitorAddress) private view returns (bool) {
+            bool result = true;
+            for(uint i = 0; i < visitors.length; i++) {
+                if (visitors[i].walletAddress == _visitorAddress) { 
+                    result = false;
+                    break;
+                }
+            }
+            return result;
+        }
 
-    function contractExpiration() statusCheck() public{
-        require(block.timestamp > expirationTime, "The Contract is not expired yet!");
-        calculateRatios();
-        if (isCancelled()){
+        // *******************************
+        // ****** PUBLIC FUNCTIONS *******
+        // *******************************
+        
+        // Oracle Function
+        function visitorLeaveCheck(uint256 _userID) public statusCheck() onlyOracle() returns(string memory){
+            uint index = checkNameSurnameExit(_userID);
+            require(index != visitorLimit + 1, "Invalid userID. Please try again!");
+            if (eventMinTime < block.timestamp){
+                visitors[index].status = VisitorStatus.STAYED;
+            }
+
+            return "The Visitor is marked as STAYED";
+        }
+
+        // Oracle Function
+        function visitorEnterCheck(uint256 _userID) public statusCheck() onlyOracle() returns(string memory){
+            uint index = checkNameSurnameEntrance(_userID);
+            require(index != visitorLimit + 1, "-- The userID is not found on the list. --");
+            visitors[index].status = VisitorStatus.ATTENDED;
+            return "++ The userID is on the list. The Visitor is marked as ATTENDED ++";
+        }
+        
+        function buyTicket () public payable statusCheck() checkSeats() checkEventStartTime() returns(uint) {
+            require(msg.value == ticketPrice, "Wrong ticket price entered. Please check the value entered and try again.");
+            require(checkAddress(msg.sender), "Only one ticket is allowed for per address");
+            uint userID = uint(keccak256(abi.encodePacked(msg.sender)));
+            visitors.push(Visitor(userID, payable(msg.sender), VisitorStatus.NOT_ATTENDED));
+            addressToHeldWei[msg.sender] += msg.value;
+            return userID;
+        }
+
+        function cancelEvent() public statusCheck() onlyOrganizer(){
             refundAllWei();
         }
-        else{
-            organizer.transfer(address(this).balance);
-            statusOfEvent = EventStatus.FINISHED;
+
+        function contractExpiration() statusCheck() public{
+            require(block.timestamp > expirationTime, "The Contract is not expired yet!");
+            calculateRatios();
+            if (isCancelled()){
+                refundAllWei();
+            }
+            else{
+                organizer.transfer(address(this).balance);
+                statusOfEvent = EventStatus.FINISHED;
+            }
+
         }
 
+        // *******************************
+        // ******* TEST FUNCTIONS ********
+        // *******************************
+        uint onlyOnceFlag = 0;
+        
+        modifier onlyOnce(){
+            require(onlyOnceFlag == 0, "Oracle is already assigned.");
+            onlyOnceFlag = 1;
+            _;
+        }
+        
+        address public oracleAddress;
+        function assignOracle() public onlyOnce() {
+            oracleAddress = msg.sender;
+        }
     }
 
-    // *******************************
-    // ******* TEST FUNCTIONS ********
-    // *******************************
-    uint onlyOnceFlag = 0;
     
-    modifier onlyOnce(){
-        require(onlyOnceFlag == 0, "Oracle is already assigned.");
-        onlyOnceFlag = 1;
-        _;
-    }
-    
-    address public oracleAddress;
-    function assignOracle() public onlyOnce() {
-        oracleAddress = msg.sender;
-    }
-}
